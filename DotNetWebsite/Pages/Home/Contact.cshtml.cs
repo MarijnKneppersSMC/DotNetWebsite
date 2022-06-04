5bf0c46e5b36b45830cae70e417031745458b3ac
Mandar1jn <Marijnkneppers@gmail.com>
2022-06-04 14:06:46 +0200

    Fixed model validation for the contect form

--- DotNetWebsite/Pages/Home/Contact.cshtml.cs
+++ DotNetWebsite/Pages/Home/Contact.cshtml.cs
@@ -1,22 +1,48 @@
-using DotNetWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace DotNetWebsite.Pages.Home
 {
-    public class ContactUsModel : PageModel
+	public class ContactUsModel : PageModel
     {
 
-        public Person Person = new Person();
+        [BindProperty(SupportsGet = true)]
+        [Required, MinLength(1)]
+        public string FirstName { get; set; } = string.Empty;
 
-		public bool success = false;
+        [BindProperty(SupportsGet = true)]
+        [Required, MinLength(1)]
+        public string LastName { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        [Required, Phone]
+        public string Telephone { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        [Required, MinLength(1)]
+        public string Message { get; set; } = string.Empty;
 
         public void OnGet()
         {
         }
 
-		public void OnPost(Person person)
+		public void OnPost(string firstName, string lastName, string email, string telephone, string address, string message)
         {
-			
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Telephone = telephone;
+            Address = address;
+            Message = message;
         }
 
     }
