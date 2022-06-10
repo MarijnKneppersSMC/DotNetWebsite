1eee66dfe082f1323f2d2f464a9cd3b3e099be91
Mandar1jn <Marijnkneppers@gmail.com>
2022-06-10 15:11:33 +0200

    Removed unused using statements

--- DotNetWebsite/Pages/Movie/Manage/Create.cshtml.cs
+++ DotNetWebsite/Pages/Movie/Manage/Create.cshtml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using DotNetWebsite.Data;
-using DotNetWebsite.Models;
 using Microsoft.AspNetCore.Authorization;
 
 namespace DotNetWebsite.Pages.Movie.Manage
--- DotNetWebsite/Pages/Movie/Manage/Delete.cshtml.cs
+++ DotNetWebsite/Pages/Movie/Manage/Delete.cshtml.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DotNetWebsite.Data;
-using DotNetWebsite.Models;
 using Microsoft.AspNetCore.Authorization;
-using System.Data;
 
 namespace DotNetWebsite.Pages.Movie.Manage
 {
